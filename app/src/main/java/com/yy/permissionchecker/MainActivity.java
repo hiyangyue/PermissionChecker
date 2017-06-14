a9b8fa7325877650e3ed5569a903661ca086934d
yangyue <yangyue@jianshu.com>
2017-06-14 20:59:57 +0800

    [feat] 添加测试代码

--- app/src/main/java/com/yy/permissionchecker/MainActivity.java
+++ app/src/main/java/com/yy/permissionchecker/MainActivity.java
@@ -1,13 +1,54 @@
 package com.yy.permissionchecker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.yy.lib.permission.BasePermissionsCallback;
+import com.yy.lib.permission.PermissionChecker;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
+    private static final String WRITE_PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    private PermissionChecker mPermissionChecker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPermissionChecker = new PermissionChecker(this);
+    }
+
+    public void test(View view) {
+        String[] permissions = new String[]{CAMERA_PERMISSION,WRITE_PERMISSION};
+        mPermissionChecker.requestPermissionList(permissions, new BasePermissionsCallback() {
+            @Override
+            public void onPermissionDefined(List<String> definedPermissions) {
+                Log.e("onPermissionDefined","  size : " + definedPermissions.size());
+            }
+
+            @Override
+            public void onAllPermissionGranted() {
+                Log.e("onAllPermissionGranted","");
+            }
+
+            @Override
+            public void onPermissionNeedExplain(String explainPermission) {
+                Log.e("onPermissionNeedExplain","   " + explainPermission);
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mPermissionChecker.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
---
+++ app/src/main/java/com/yy/permissionchecker/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.yy.permissionchecker;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * author : yueyang
+ * date : 2017/6/13
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_second);
+    }
+}
